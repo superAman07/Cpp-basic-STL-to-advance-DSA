d762f7d68d9641f155d23594d38b091e2001589c
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2023-11-19 23:04:20 +0530

    Add files via upload

---
+++ 111.Largest Element in array.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[]{5,8,20,10};
+    make_heap(arr,arr+4);
+    cout<<arr[0];
+    return 0;
+}
+
+
+
+
+
+
+ // O(n) time complexity
+// #include<bits/stdc++.h>
+// using namespace std;
+// int max(int arr[],int n){
+//     int res=0;
+//     for(int i=0;i<n;i++){
+//         if(arr[i]>arr[res]){
+//             res=i;
+//         }
+//     }
+//     return res;
+// }
+// int main(){
+//     int arr[]{5,8,20,10};
+//     int n=sizeof(arr)/sizeof(arr[0]);
+//     cout<<arr[max(arr,n)];
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+//naive solution
+// #include<bits/stdc++.h>
+// using namespace std;
+// int LargestElement(int arr[],int n){
+//     for(int i=0;i<n;i++){
+//        bool flag=true;
+//         for(int j=0;j<n;j++){
+//             if(arr[j]>arr[i]){
+//                 flag=false;
+//                 break;
+//             }
+//         }
+//         if(flag==true){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+// int main(){
+//     int arr[]{5,8,20,10};
+//     int n=sizeof(arr)/sizeof(arr[0]);
+//     cout<<LargestElement(arr,n)<<"\n";
+//     cout<<arr[LargestElement(arr,n)];
+//     return 0;
+// }
---
+++ 112.Remove_same_elements_from_array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+using namespace std;
+void ReverseArray(vector<int>&v){
+    int n=v.size();
+    set<int>s;
+    for(int i=0;i<n;i++){
+        s.insert(v[i]);
+    }
+    for(auto it = s.begin();it!=s.end();++it){
+        cout<<*it<<" ";
+    }
+    int x = n - s.size();
+    for(int i=0;i<x;i++){
+        cout<<"0 ";
+    }
+}
+int main(){
+    vector<int>v{10,20,20,30,30,30,30};
+    ReverseArray(v);
+    return 0;
+}
