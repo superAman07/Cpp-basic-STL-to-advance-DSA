c6f26a38297aecf6c920f115eebc38772f9bdf84
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2023-12-05 23:07:33 +0530

    02.iterators in C++.cpp

--- 02.iterators in C++.cpp
+++ 02.iterators in C++.cpp
@@ -1,35 +1,35 @@
      // begin()-end() function
 
-// #include<iostream>
-// #include<vector>
-// using namespace std;
-// int main(){
-//     vector<int>v {10,20,30,40,70};
-//     vector<int>::iterator i=v.begin();
-//     cout<<(*i)<<"\n";
-//     i+=1;
-//     cout<<(*i)<<"\n";
-//     i=v.end();
-//     i--;
-//     cout<<(*i)<<"\n";
-//     return 0;
-// }
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>v {10,20,30,40,70};
+    vector<int>::iterator i=v.begin();
+    cout<<(*i)<<"\n";
+    i+=1;
+    cout<<(*i)<<"\n";
+    i=v.end();
+    i--;
+    cout<<(*i)<<"\n";
+    return 0;
+}
      
      //next()-prev() function
 
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main(){
-//     vector<int>v {10,20,30,40,70};
-//     auto i=v.begin();
-//     i=next(i);
-//     cout<<(*i)<<"\n";
-//     i+=2;
-//     cout<<(*i)<<"\n";
-//     i=prev(i);
-//     cout<<(*i)<<"\n";
-//     return 0;
-// }
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>v {10,20,30,40,70};
+    auto i=v.begin();
+    i=next(i);
+    cout<<(*i)<<"\n";
+    i+=2;
+    cout<<(*i)<<"\n";
+    i=prev(i);
+    cout<<(*i)<<"\n";
+    return 0;
+}
 
      // advance() function
 
