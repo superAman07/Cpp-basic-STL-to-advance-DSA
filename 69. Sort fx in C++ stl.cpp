e9b44bee566c918eaeb6426d6967e1521c8b268e
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2023-12-17 18:15:29 +0530

    69. Sort fx in C++ stl.cpp

--- 69. Sort fx in C++ stl.cpp
+++ 69. Sort fx in C++ stl.cpp
@@ -1,47 +1,47 @@
-// #include<iostream>
-// #include<algorithm>
-// using namespace std;
-// int main(){
-//     int arr[]{38,29,10,17,19};
-//     int n = sizeof(arr)/sizeof(arr[0]);
-//     sort(arr,arr+n);
-//     cout<<"Increasing order: ";
-//     for(auto i:arr){
-//         cout<<i<<" ";
-//     }
-//     sort(arr,arr+n,greater());
-//     cout<<"\nDecreasing order:";
-//     for(int i:arr){
-//         cout<<i<<" ";
-//     }
-//     return 0;
-// }
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int arr[]{38,29,10,17,19};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    sort(arr,arr+n);
+    cout<<"Increasing order: ";
+    for(auto i:arr){
+        cout<<i<<" ";
+    }
+    sort(arr,arr+n,greater());
+    cout<<"\nDecreasing order:";
+    for(int i:arr){
+        cout<<i<<" ";
+    }
+    return 0;
+}
 
 
 // Vector
 
-// #include<bits/stdc++.h>
-// #include<iostream>
-// #include<vector>
-// #include<algorithm>
-// using namespace std;
-// void func(vector<int>&v){
-//     sort(v.begin(),v.end());
-//     cout<<"Increasing Order:";
-//     for(auto i:v){
-//         cout<<i<<" ";
-//     }
-//     cout<<"\n Decreasing order:";
-//     sort(v.begin(),v.end(),greater());
-//     for(auto i:v){
-//         cout<<i<<" ";
-//     }
-// }
-// int main(){
-//     vector<int>v{13,19,20,29,24,50};
-//     func(v);
-//     return 0;
-// }
+#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+void func(vector<int>&v){
+    sort(v.begin(),v.end());
+    cout<<"Increasing Order:";
+    for(auto i:v){
+        cout<<i<<" ";
+    }
+    cout<<"\n Decreasing order:";
+    sort(v.begin(),v.end(),greater());
+    for(auto i:v){
+        cout<<i<<" ";
+    }
+}
+int main(){
+    vector<int>v{13,19,20,29,24,50};
+    func(v);
+    return 0;
+}
 
 #include<bits/stdc++.h>
 using namespace std;
