c420615bbb4c0dc7ceddf4a3cfbdaf8e3cf261cb
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2023-12-18 17:16:37 +0530

    19. stack in C++ STl.cpp

--- 19. stack in C++ STl.cpp
+++ 19. stack in C++ STl.cpp
@@ -14,32 +14,32 @@ int main(){
     return 0;
 }
 
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main(){
-//     stack<int>s;
-//     s.push(7);
-//     s.push(10);
-//     s.push(13);
-//     while(!s.empty()){
-//         cout<<s.top()<<" ";
-//         s.pop();
-//     }
-//     return 0;
-// }
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    stack<int>s;
+    s.push(7);
+    s.push(10);
+    s.push(13);
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    return 0;
+}
 
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main(){
-//     stack<char>s;
-//     s.push('a');
-//     s.push('b');
-//     s.push('c');
-//     s.push('e');
-//     s.push('d');
-//     while(!s.empty()){
-//         cout<<s.top()<<" ";
-//         s.pop();
-//     }
-//     return 0;
-// }
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    stack<char>s;
+    s.push('a');
+    s.push('b');
+    s.push('c');
+    s.push('e');
+    s.push('d');
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    return 0;
+}
