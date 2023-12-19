5d88aa594891d6c7030e7277da097bc6fb2126b6
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2023-12-19 23:09:43 +0530

    practiceProblem.cpp

--- practiceProblem.cpp
+++ practiceProblem.cpp
@@ -1,24 +1,24 @@
 //Sorting of one array acc.. to other
 
-// #include<bits/stdc++.h>
-// using namespace std;
-// void sort_arr_wrt_other(int a[],char b[],int n){
-//     pair<int,char>pa[n];
-//     for(int i=0;i<n;i++){
-//         pa[i]={a[i],b[i]};
-//     }
-//     sort(pa,pa+n);
-//     for(int i=0;i<n;i++){
-//         cout<<pa[i].second<<" ";
-//     }
-// }
-// int main(){
-//     int n=3;
-//     int a[]{10,15,5};
-//     char b[]{'X','Y','Z'};
-//     sort_arr_wrt_other(a,b,n);
-//     return 0;
-// }
+#include<bits/stdc++.h>
+using namespace std;
+void sort_arr_wrt_other(int a[],char b[],int n){
+    pair<int,char>pa[n];
+    for(int i=0;i<n;i++){
+        pa[i]={a[i],b[i]};
+    }
+    sort(pa,pa+n);
+    for(int i=0;i<n;i++){
+        cout<<pa[i].second<<" ";
+    }
+}
+int main(){
+    int n=3;
+    int a[]{10,15,5};
+    char b[]{'X','Y','Z'};
+    sort_arr_wrt_other(a,b,n);
+    return 0;
+}
 
 
 // #include<bits/stdc++.h>
