28c06d3944463931cd40708f9b8c8b8b792d5abe
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2023-12-21 16:27:59 +0530

    47. Print repeating elements.cpp

--- 47. Print repeating elements.cpp
+++ 47. Print repeating elements.cpp
@@ -1,28 +1,28 @@
    // Naive solution
 
-// #include<bits/stdc++.h>
-// using namespace std;
-// void repeating_element(int arr[],int n){
-//     for(int i=0;i<n;i++){
-//         int flag=0;
-//         for(int j=i+1;j<n;j++){
-//             if(arr[i]==arr[j]){           //O(n^2)
-//                 flag=1;
-//                 break;
-//             }
-//         }
-//         if(flag==1){
-//             cout<<arr[i]<<" ";
-//         }
-//     }
-// }
-// int main(){
-//     int arr[]{10,8,10,7,7,6};
-//     // int arr[]{2,2,2,2};
-//     int n=sizeof(arr)/sizeof(arr[0]);
-//     repeating_element(arr,n);
-//     return 0;
-// }
+#include<bits/stdc++.h>
+using namespace std;
+void repeating_element(int arr[],int n){
+    for(int i=0;i<n;i++){
+        int flag=0;
+        for(int j=i+1;j<n;j++){
+            if(arr[i]==arr[j]){           //O(n^2)
+                flag=1;
+                break;
+            }
+        }
+        if(flag==1){
+            cout<<arr[i]<<" ";
+        }
+    }
+}
+int main(){
+    int arr[]{10,8,10,7,7,6};
+    // int arr[]{2,2,2,2};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    repeating_element(arr,n);
+    return 0;
+}
 
    // efficient solution
 
