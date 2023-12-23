a2b62f44b3c0c98113523238743a70c51fc3a79f
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2023-12-23 11:11:51 +0530

    57. Count disitinct elements in every window.cpp

--- 57. Count dsitinct elements in every window.cpp
+++ 57. Count dsitinct elements in every window.cpp
@@ -1,33 +1,33 @@
   // naive solution   O((n-k)*k*k)
 
-// #include<iostream>
-// #include<bits/stdc++.h>
-// using namespace std;
-// void count_distinct_int_every_window(vector<int>&v,int k){
-//     int n=v.size();
-//     for(int i=0;i<n-k+1;i++){
-//         int count=0;
-//         for(int j=0;j<k;j++){
-//             bool flag=false;
-//             for(int p=0;p<j;p++){
-//                 if(v[i+j]==v[p+i]){
-//                     flag=true;
-//                     break;
-//                 }
-//             }
-//             if(flag==false){
-//                 count++;
-//             }
-//         }
-//         cout<<count<<" ";
-//     }
-// }
-// int main(){
-//     vector<int>v{10,20,20,10,30,40,10};
-//     int k=4;
-//     count_distinct_int_every_window(v,k);
-//     return 0;
-// }
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void count_distinct_int_every_window(vector<int>&v,int k){
+    int n=v.size();
+    for(int i=0;i<n-k+1;i++){
+        int count=0;
+        for(int j=0;j<k;j++){
+            bool flag=false;
+            for(int p=0;p<j;p++){
+                if(v[i+j]==v[p+i]){
+                    flag=true;
+                    break;
+                }
+            }
+            if(flag==false){
+                count++;
+            }
+        }
+        cout<<count<<" ";
+    }
+}
+int main(){
+    vector<int>v{10,20,20,10,30,40,10};
+    int k=4;
+    count_distinct_int_every_window(v,k);
+    return 0;
+}
 
 
     // O(n)
