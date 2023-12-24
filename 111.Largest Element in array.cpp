eea8aca9c4ef536c3eff8dcfbc23adeeb8630ced
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2023-12-24 19:27:24 +0530

    111.Largest Element in array.cpp

--- 111.Largest Element in array.cpp
+++ 111.Largest Element in array.cpp
@@ -13,23 +13,23 @@ int main(){
 
 
  // O(n) time complexity
-// #include<bits/stdc++.h>
-// using namespace std;
-// int max(int arr[],int n){
-//     int res=0;
-//     for(int i=0;i<n;i++){
-//         if(arr[i]>arr[res]){
-//             res=i;
-//         }
-//     }
-//     return res;
-// }
-// int main(){
-//     int arr[]{5,8,20,10};
-//     int n=sizeof(arr)/sizeof(arr[0]);
-//     cout<<arr[max(arr,n)];
-//     return 0;
-// }
+#include<bits/stdc++.h>
+using namespace std;
+int max(int arr[],int n){
+    int res=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]>arr[res]){
+            res=i;
+        }
+    }
+    return res;
+}
+int main(){
+    int arr[]{5,8,20,10};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    cout<<arr[max(arr,n)];
+    return 0;
+}
 
 
 
@@ -39,27 +39,27 @@ int main(){
 
 
 //naive solution
-// #include<bits/stdc++.h>
-// using namespace std;
-// int LargestElement(int arr[],int n){
-//     for(int i=0;i<n;i++){
-//        bool flag=true;
-//         for(int j=0;j<n;j++){
-//             if(arr[j]>arr[i]){
-//                 flag=false;
-//                 break;
-//             }
-//         }
-//         if(flag==true){
-//             return i;
-//         }
-//     }
-//     return -1;
-// }
-// int main(){
-//     int arr[]{5,8,20,10};
-//     int n=sizeof(arr)/sizeof(arr[0]);
-//     cout<<LargestElement(arr,n)<<"\n";
-//     cout<<arr[LargestElement(arr,n)];
-//     return 0;
-// }
+#include<bits/stdc++.h>
+using namespace std;
+int LargestElement(int arr[],int n){
+    for(int i=0;i<n;i++){
+       bool flag=true;
+        for(int j=0;j<n;j++){
+            if(arr[j]>arr[i]){
+                flag=false;
+                break;
+            }
+        }
+        if(flag==true){
+            return i;
+        }
+    }
+    return -1;
+}
+int main(){
+    int arr[]{5,8,20,10};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    cout<<LargestElement(arr,n)<<"\n";
+    cout<<arr[LargestElement(arr,n)];
+    return 0;
+}
