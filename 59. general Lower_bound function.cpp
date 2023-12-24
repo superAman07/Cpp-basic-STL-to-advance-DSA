996e0edb1ed519a724c8003ccc3c33a821ae80f1
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2023-12-24 18:36:45 +0530

    59. general Lower_bound function.cpp

--- 59. general Lower_bound function.cpp
+++ 59. general Lower_bound function.cpp
@@ -1,34 +1,34 @@
   // vector
 
-// #include<iostream>
-// #include<algorithm>
-// using namespace std;
-// int main(){
-//     vector<int>v{10,20,20,20,30,40};
-//     auto it=lower_bound(v.begin(),v.end(),20);
-//     cout<<*it<<" ";
-//     it=lower_bound(v.begin(),v.end(),25);
-//     cout<<*it<<" ";
-//     it=lower_bound(v.begin(),v.end(),50);
-//     cout<<"Element Before last i.e., v.end() "<<*it<<" and index is "<<it-v.begin()<<"\n";  
-//     return 0;
-// }
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    vector<int>v{10,20,20,20,30,40};
+    auto it=lower_bound(v.begin(),v.end(),20);
+    cout<<*it<<" ";
+    it=lower_bound(v.begin(),v.end(),25);
+    cout<<*it<<" ";
+    it=lower_bound(v.begin(),v.end(),50);
+    cout<<"Element Before last i.e., v.end() "<<*it<<" and index is "<<it-v.begin()<<"\n";  
+    return 0;
+}
 
     // array
 
-// #include<iostream>
-// #include<algorithm>
-// using namespace std;
-// int main(){
-//     int arr[]{10,20,20,20,30,40};
-//     auto it=lower_bound(arr,arr+6,20);
-//     cout<<*it<<" ";
-//     it=lower_bound(arr,arr+6,25);
-//     cout<<(*it)<<" ";
-//     it=lower_bound(arr,arr+6,50);
-//     cout<<"\nElement is at last in array "<<*it<<" position is "<<it-arr;
-//     return 0;
-// }
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int arr[]{10,20,20,20,30,40};
+    auto it=lower_bound(arr,arr+6,20);
+    cout<<*it<<" ";
+    it=lower_bound(arr,arr+6,25);
+    cout<<(*it)<<" ";
+    it=lower_bound(arr,arr+6,50);
+    cout<<"\nElement is at last in array "<<*it<<" position is "<<it-arr;
+    return 0;
+}
 
 
    // BST
