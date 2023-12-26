b1793d7233a37ec9c173f66ea7ce1d97809cea21
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2023-12-26 17:28:26 +0530

    28. Reverse the first k elements in queue.cpp

--- 28. Reverse the first k elements in queue.cpp
+++ 28. Reverse the first k elements in queue.cpp
@@ -34,39 +34,39 @@ int main(){
 
       // With the help of stack
     
-// #include<bits/stdc++.h>
-// #include<stack>
-// #include<queue>
-// using namespace std;
+#include<bits/stdc++.h>
+#include<stack>
+#include<queue>
+using namespace std;
 
-// void reverse_k_elements(stack<int>&q,int k){
-//     stack<int>s;
-//     for(int i=0;i<k;i++){
-//         s.push(q.front());
-//         q.pop();
-//     }
-//     while(!s.empty()){
-//         q.push(s.top());
-//         s.pop();
-//     }
-//     for(int i=0;i<(q.size()-k);i++){
-//         q.push(q.front());
-//         q.pop();
-//     }
-//     for(auto x:q){
-//         cout<<x<<" ";
-//     }
-// }
-// int main(){
-//     queue<int>q;
-//     q.push(10);
-//     q.push(20);
-//     q.push(30);
-//     q.push(40);
-//     q.push(50);
-//     int k;
-//     cout<<"Enter the value of k:";
-//     cin>>k;
-//     reverse_k_elements(q,k);
-//     return 0;
-// }
+void reverse_k_elements(stack<int>&q,int k){
+    stack<int>s;
+    for(int i=0;i<k;i++){
+        s.push(q.front());
+        q.pop();
+    }
+    while(!s.empty()){
+        q.push(s.top());
+        s.pop();
+    }
+    for(int i=0;i<(q.size()-k);i++){
+        q.push(q.front());
+        q.pop();
+    }
+    for(auto x:q){
+        cout<<x<<" ";
+    }
+}
+int main(){
+    queue<int>q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    int k;
+    cout<<"Enter the value of k:";
+    cin>>k;
+    reverse_k_elements(q,k);
+    return 0;
+}
