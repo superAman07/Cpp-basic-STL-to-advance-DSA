d84e12b385cd2a95850757162228fe9dfd5d8e6c
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2023-12-26 17:26:25 +0530

    24. Next greatest element.cpp

--- 24. Next greatest element.cpp
+++ 24. Next greatest element.cpp
@@ -21,32 +21,32 @@ int main(){
 
     // With the help of vector we can get the output in desired way
 
-// #include<bits/stdc++.h>
-// #include<iostream>
-// #include<vector>
-// #include<stack>
-// using namespace std;
-// vector<int>nextGreater(vector<int>&arr){
-//     vector<int>v;
-//     int n=arr.size();
-//     stack<int>s;
-//     s.push(arr[n-1]);
-//     for(int i=n-2;i>=0;i--){
-//         while(!s.empty() && (arr[i]>=s.top())){
-//             s.pop();
-//         }
-//         int result=(s.empty())?(-1):(s.top());
-//         v.push_back(result);
-//         s.push(arr[i]);
-//     }
-//     reverse(v.begin(),v.end());
-//     return v;
-// }
-// int main(){
-//     vector<int>arr{3,5,1,6,4,7,9,2};
-//     vector<int>result=nextGreater(arr);
-//     for(auto x:result){
-//         cout<<x<<" ";
-//     }
-//     return 0;
-// }
+#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+vector<int>nextGreater(vector<int>&arr){
+    vector<int>v;
+    int n=arr.size();
+    stack<int>s;
+    s.push(arr[n-1]);
+    for(int i=n-2;i>=0;i--){
+        while(!s.empty() && (arr[i]>=s.top())){
+            s.pop();
+        }
+        int result=(s.empty())?(-1):(s.top());
+        v.push_back(result);
+        s.push(arr[i]);
+    }
+    reverse(v.begin(),v.end());
+    return v;
+}
+int main(){
+    vector<int>arr{3,5,1,6,4,7,9,2};
+    vector<int>result=nextGreater(arr);
+    for(auto x:result){
+        cout<<x<<" ";
+    }
+    return 0;
+}
