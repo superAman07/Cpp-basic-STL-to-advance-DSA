367d077f8198af4f0186a1b1ad8a5aa1ee47cd62
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2023-12-26 16:45:18 +0530

    116.Index of last occurence.cpp

--- 116.Index of last occurence.cpp
+++ 116.Index of last occurence.cpp
@@ -1,26 +1,26 @@
 //T.C. O(n)
 
-// #include<iostream>
-// // #include<vector>
-// using namespace std;
-// int lastIndexOccurence(int arr[],int n, int k){
-//     for(int i=n-1;i>=0;i--){
-//         if(arr[i]==k){
-//             return i;
-//         }
-//     }
-//     return 0;
-// }
-// int main(){
-//     int n,k;
-//     cin>>n>>k;
-//     int arr[n];
-//     for(int i=0;i<n;i++){
-//         cin>>arr[i];
-//     }
-//     cout<<lastIndexOccurence(arr,n,k);
-//     return 0;
-// }
+#include<iostream>
+#include<vector>
+using namespace std;
+int lastIndexOccurence(int arr[],int n, int k){
+    for(int i=n-1;i>=0;i--){
+        if(arr[i]==k){
+            return i;
+        }
+    }
+    return 0;
+}
+int main(){
+    int n,k;
+    cin>>n>>k;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<lastIndexOccurence(arr,n,k);
+    return 0;
+}
 
 // T.C. O(logn) using binary search
 
