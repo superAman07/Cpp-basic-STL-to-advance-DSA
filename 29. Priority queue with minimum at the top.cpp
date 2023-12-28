f65ead04ab23c9855b6cb9c2c6bf3218fecb7fa6
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2023-12-28 15:56:25 +0530

    29. Priority queue with minimum at the top.cpp

--- 29. Priority queue with minimum at the top.cpp
+++ 29. Priority queue with minimum at the top.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main(){
     int arr[]{10,1,7};
-    for(int i=0;i<sizeof(arr);i++){   // for minimum at the top
+    for(int i=0;i<sizeof(arr);i++){   
         arr[i]=-arr[i];                
     }
     priority_queue<int>pq(arr,arr+3);
