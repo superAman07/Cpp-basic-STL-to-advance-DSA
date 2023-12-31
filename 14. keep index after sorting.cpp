b8292dfd658bcb967885e2ed1838dbe146727bb1
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2023-12-31 18:22:53 +0530

    14. keep index after sorting.cpp

--- 14. keep index after sorting.cpp
+++ 14. keep index after sorting.cpp
@@ -3,7 +3,8 @@ using namespace std;
 void smalltogreater(int a[],int n){
     vector<pair<int,int>>b;
     for(int i=0;i<n;i++){
-        b.push_back({a[i],i});  // b.push_back(make_pair(a[i],i));
+        // b.push_back({a[i],i});  //
+        b.push_back(make_pair(a[i],i));
         sort(b.begin(),b.end());
         for(auto x:b){
             cout<<x.first<<" "<<x.second<<"\n";
