de791c68143590355ac7216664eef8688a6ddb36
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-01-03 17:58:57 +0530

    100. Count set bit.cpp

--- 100. Count set bit.cpp
+++ 100. Count set bit.cpp
@@ -21,22 +21,22 @@
 
 // Brian Kerningam's Algorithm
 // O(set bit count)
-// #include<bits/stdc++.h>
-// using namespace std;
-// int countBits(int n){
-//     int res=0;
-//     while(n>0){
-//         n=(n&(n-1));
-//         res++;
-//     }
-//     return res;
-// }
-// int main(){
-//     int n;
-//     cin>>n;
-//     cout<<countBits(n);
-//     return 0;
-// }
+#include<bits/stdc++.h>
+using namespace std;
+int countBits(int n){
+    int res=0;
+    while(n>0){
+        n=(n&(n-1));
+        res++;
+    }
+    return res;
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<countBits(n);
+    return 0;
+}
 
 // best solution
 
