03d772f998b681d8ef0616b2200a1e96526c8474
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-01-04 14:50:51 +0530

    105.Tail_Recursion_exercises.cpp

--- 105.Tail_Recursion_exercises.cpp
+++ 105.Tail_Recursion_exercises.cpp
@@ -13,16 +13,16 @@ int main(){
 }
 // //o/p:- 1 2 3 4 5
 
-// #include<iostream>
-// using namespace std;
-// int fact(int n){
-//     if(n==0 || n==1)return 1;
-//     return n*fact(n-1);
-// }
-// int main(){
-//     cout<<fact(5);
-//     return 0;
-// }
+#include<iostream>
+using namespace std;
+int fact(int n){
+    if(n==0 || n==1)return 1;
+    return n*fact(n-1);
+}
+int main(){
+    cout<<fact(5);
+    return 0;
+}
 
 
 
