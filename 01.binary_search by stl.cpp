13186e711024ceeb814429fda859e60798ce5591
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-02-09 11:33:39 +0530

    01.binary_search by stl.cpp

--- 01.binary_search by stl.cpp
+++ 01.binary_search by stl.cpp
@@ -16,3 +16,50 @@ int main(){
     binary_fun(arr,n);
     return 0;
 }
+
+
+//Naive Solution O(n^1/2)
+
+// #include<iostream>
+// using namespace std;
+// int sqRoot(int n){
+//     int i=1;
+//     while(i*i<=n){
+//         i++;
+//     }
+//     return (i-1);
+// }
+// int main(){
+//     int x;
+//     cin>>x;
+//     cout<<sqRoot(x);
+//     return 0;
+// }
+
+// Efficient solution using Binary Search
+
+#include<iostream>
+using namespace std;
+int sqRoot(int n){
+    int low = 1, high = n,ans = -1;
+    while(low<=high){
+        int mid = low + (high-low)/2;
+        if(mid*mid==n){
+            return mid;
+        }
+        else if(mid*mid<n){
+            low = mid+1;
+            ans = mid;
+        }
+        else{
+            high = mid-1;
+        }
+    }
+    return ans;
+}
+int main(){
+    int x;
+    cin>>x;
+    cout<<sqRoot(x);
+    return 0;
+}
