c9e38f2e55e03562c86d39f158d63436dcdd4062
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-02-14 10:48:38 +0530

    122. Repeating element with conditions.cpp

---
+++ 122. Repeating element with conditions.cpp
@@ -0,0 +1,40 @@
+// Repeating element Conditions arr size >=2, only one element is repeats and all
+// elements from 0 to max(arr) are present
+
+//O(n) naive solution
+
+// #include<iostream>
+// #include<algorithm>
+// using namespace std;
+// int main(){
+//     int arr[]{0,2,1,3,2,2};
+//     int n = sizeof(arr)/sizeof(arr[0]);
+//     sort(arr,arr+n);
+//     for(int i=0;i<n;i++){
+//         if(arr[i]==arr[i+1]){
+//             cout<< arr[i];
+//             break;
+//         }
+//     }
+//     return 0;
+// }
+
+// Efficient approach in O(n)
+#include<iostream>
+using namespace std;
+int repeatedElem(int arr[], int n){
+    bool visited[]{false,false,false,false,false,false};
+    for(int i=0;i<n;i++){
+        if(visited[arr[i]]==true){
+            return arr[i];
+        }
+        visited[arr[i]]=true;
+    }
+    return 0;
+}
+int main(){
+    int arr[]{0,2,1,3,2,2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<repeatedElem(arr,n);
+    return 0;
+}
