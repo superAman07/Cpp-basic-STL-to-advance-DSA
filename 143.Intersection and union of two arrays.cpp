91f594a950275ab6be417de38e1f3a14c366fa6b
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-03-14 19:10:32 +0530

    143.Intersection and union of two arrays.cpp

--- 143.Intersection and union of two arrays.cpp
+++ 143.Intersection and union of two arrays.cpp
@@ -1,3 +1,5 @@
+//intersection of two arrays
+
 #include<iostream>
 #include<unordered_set>
 using namespace std;
@@ -21,5 +23,29 @@ int main(){
     int m= sizeof(a)/sizeof(a[0]);
     int n= sizeof(b)/sizeof(b[0]);
     cout<<intersection(a,b,m,n);
+    return 0;
+}
+// union of two array
+
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+unordered_set<int>s;
+int Union(int a[],int b[],int m,int n){
+    for(int i=0;i<max(m,n);i++){
+        s.insert(a[i]);
+        s.insert(b[i]);
+    }
+    return s.size();
+}
+int main(){
+    int a[]{1,12,15,10};
+    int b[]{12,13,14,15};
+    int m=sizeof(a)/sizeof(a[0]);
+    int n=sizeof(b)/sizeof(b[0]);
+    cout<<"The merged array is: "<<Union(a,b,m,n);
+    for(auto &i:s){
+        cout<<i<<" ";
+    }
     return 0;
 }
