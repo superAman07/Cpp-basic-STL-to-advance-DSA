feb7201ad646138f83e06747ed9e404d4c3c52af
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-04-17 15:44:44 +0530

    171.Removing first element in DLL.cpp

---
+++ 171.Removing first element in DLL.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+struct node{
+    int data;
+    node* next;
+    node* prev;
+    node(int x){
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+};
+node *RemoveFirstDLL(node* head){
+    if(head==NULL){
+        return NULL;
+    }
+    node* ptr=head;
+    ptr= ptr->next;
+    if(ptr!=NULL){
+        ptr->next->prev=NULL;
+    }
+    delete(head); 
+    return ptr;
+}
+int main(){
+    node* head=new node(10);
+    node* second=new node(20);
+    node* third=new node(30);
+    node* fourth=new node(40);
+    head->prev=NULL;
+    head->next=second;
+    second->next=third;
+    second->prev=head;
+    third->next=fourth;
+    third->prev=second;
+    fourth->next=NULL;
+    fourth->prev=third;
+   head= RemoveFirstDLL(head);
+    node* ptr=head;
+    while(ptr!=NULL){
+        cout<<ptr->data<<" ";
+        ptr=ptr->next;
+    }
+    return 0;
+}
