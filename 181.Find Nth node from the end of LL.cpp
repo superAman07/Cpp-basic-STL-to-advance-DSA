e34e7d2731a7a5f4861d0cdd1f6f9bc6c0cb8fec
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-04-30 01:28:56 +0530

    181.Find Nth node from the end of LL.cpp

--- 181.Find Nth node from the end of LL.cpp
+++ 181.Find Nth node from the end of LL.cpp
@@ -8,20 +8,37 @@ struct node{
         next=NULL;
     }
 };
+// Using length of LL
+// void findNthFromLast(node* head,int n){
+//     node* ptr=head;
+//     int count=0;
+//     while(ptr!=NULL){
+//         count++;
+//         ptr=ptr->next;
+//     }
+//     if(count<n)return;
+//     node* curr=head;
+//     int i=0;
+//     while(i<count-n){
+//         curr=curr->next;
+//         i++;
+//     }
+//     cout<<curr->data;
+// }
+// using two pointer/Reference
 void findNthFromLast(node* head,int n){
-    node* ptr=head;
-    int count=0;
-    while(ptr!=NULL){
-        count++;
-        ptr=ptr->next;
+    if(head==NULL)return;
+    node* first=head;
+    for(int i=0;i<n;i++){
+        if(!first)return ;
+        first=first->next;
     }
-    node* curr=head;
-    int i=0;
-    while(i<count-n){
-        curr=curr->next;
-        i++;
+    node* second=head;
+    while(first!=NULL){
+        first=first->next;
+        second=second->next;
     }
-    cout<<curr->data;
+    cout<<second->data;
 }
 int main(){
     node* head=new node(10);
@@ -35,6 +52,6 @@ int main(){
     third->next=fourth;
     fourth->next=fifth;
     fifth->next=sixth;
-    findNthFromLast(head,3);
+    findNthFromLast(head,2);
     return 0;
 }
