efca241c91895773cc85376e6932868b362aca24
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-05-01 00:44:31 +0530

    182.Reverse given LL.cpp

---
+++ 182.Reverse given LL.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+struct node{
+    int data;
+    node* next;
+    node(int x){
+        data=x;
+        next=NULL;
+    }
+};
+//Naive solution function
+node* reverseLL(node* head){
+    vector<int>v;
+    for(node* curr=head;curr!=NULL;curr=curr->next){
+        v.push_back(curr->data);
+    } 
+    for(node* curr=head;curr!=NULL;curr=curr->next){
+        curr->data=v.back(); 
+        v.pop_back();
+    }
+    return head;
+}
+int main(){
+    node* head=new node(10);
+    head->next=new node(20);
+    head->next->next=new node(30);
+    head->next->next->next=new node(40);
+    head->next->next->next->next=NULL;
+    node* ptr=head;
+    cout<<"Before reverse: ";
+    while(ptr!=NULL){
+        cout<<ptr->data<<" ";
+        ptr=ptr->next;
+    }
+    head=reverseLL(head);
+    ptr=head;
+    cout<<"\nAfter reverse: ";
+    while(ptr!=NULL){
+        cout<<ptr->data<<" ";
+        ptr=ptr->next;
+    }
+    return 0;
+}
