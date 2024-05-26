fc5951d9d7d2f447498122cf20413dcd1432a6b3
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-05-26 21:54:08 +0530

    194.Check palindrome in LL.cpp

--- 194.Check palindrome in LL.cpp
+++ 194.Check palindrome in LL.cpp
@@ -9,7 +9,7 @@ struct node{
         next=NULL;
     }
 };
-// Naive solution
+// Naive solution in O(n) time and O(1) space;
 bool isPalindromeNaive(node* head){
     stack<char>st;
     for(node* curr=head;curr!=NULL;curr=curr->next){
@@ -23,12 +23,38 @@ bool isPalindromeNaive(node* head){
     }
     return true;
 }
+// Efficient approach with O(n) time and O(1) space;
+node* reverseLL(node* head){
+    if(head==NULL||head->next==NULL)return NULL;
+    node* rest_head=reverseLL(head->next);
+    node* rest_tail=head->next;
+    rest_tail->next=head;
+    head->next=head;
+    return rest_head;     
+}
+bool isPalindrome(node* head){
+    if(head==NULL)return true;
+    node* slow=head,*fast=head;
+    while(fast->next!=NULL && fast->next->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    node* rev=reverseLL(slow->next);
+    node* curr=head;
+    while(rev!=NULL){
+        if(rev->data!=curr->data)return false;
+        rev=rev->next;
+        curr=curr->next;
+    }
+    return true;
+}
 int main(){
     node* head=new node('c');
     head->next=new node('i');
     head->next->next=new node('v');
     head->next->next->next=new node('i');
     head->next->next->next->next=new node('c');
-    cout<<boolalpha<<isPalindromeNaive(head);
+    // cout<<boolalpha<<isPalindromeNaive(head);
+    cout<<boolalpha<<isPalindrome(head);
     return 0;
 }
