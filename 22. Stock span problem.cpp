9397badaea2c0271c774fe0c65d021ece25ce57d
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-07-02 00:04:56 +0530

    22. Stock span problem.cpp

--- 22. Stock span problem.cpp
+++ 22. Stock span problem.cpp
@@ -1,27 +1,30 @@
-#include<bits/stdc++.h>
-using namespace std;
-void Spanning(vector<int>&v){
-    int n=v.size();
-    for(int i=0;i<n;i++){
-        int count=1;
-        for(int j=i-1;j>=0;j--){                      // O(n^2)
-            if(v[j]<=v[i]){
-                count++;
-            }
-            else{
-                break;
-            }
-        }
-        cout<<count<<" ";
-    }
-}
-int main(){
-    vector<int>v{15,13,12,14,16,8,6,4,10,30};
-    Spanning(v);
-    return 0;
-}
+// #include<bits/stdc++.h>
+// using namespace std;
+// void Spanning(vector<int>&v){
+//     int n=v.size();
+//     for(int i=0;i<n;i++){
+//         int count=1;
+//        // for(int j=i-1;j>=0;j--){                      // O(n^2)
+//        //     if(v[j]<=v[i]){
+//        //         count++;
+//        //     }
+//        //     else{
+//        //         break;
+//        //     }
+//        // }
+//        for(int j=i-1;j>0&&v[j]<=v[i];j--){
+//         count++;
+//        }
+//         cout<<count<<" ";
+//     }
+// }
+// int main(){
+//     vector<int>v{15,13,12,14,16,8,6,4,10,30};
+//     Spanning(v);
+//     return 0;
+// }
 
-     // Efficient solution
+     // Efficient solution in O(n)
     
 #include<bits/stdc++.h>
 using namespace std;
@@ -32,9 +35,9 @@ void span_effeciently(vector<int>&v){
         while(!s.empty() && v[i]>=v[s.top()]){
             s.pop();
         }
-    int span=(s.empty())?(i+1):(i-s.top());
-    cout<<span<<" ";
-    s.push(i);
+        int span=(s.empty())?(i+1):(i-s.top());
+        cout<<span<<" ";
+        s.push(i);
     }
 }
 int main(){
