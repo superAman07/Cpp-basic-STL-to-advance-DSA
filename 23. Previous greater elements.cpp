f74c95b762630d8f852cd1dcb5c65da1fd816af9
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-07-03 20:39:26 +0530

    23. Previous greater elements.cpp

--- 23. Previous greater elements.cpp
+++ 23. Previous greater elements.cpp
@@ -1,46 +1,82 @@
-#include<bits/stdc++.h>
-using namespace std;
-void prevoiusGreater(vector<int>&arr){
-    int n=arr.size();
-    for(int i=0;i<n;i++){
-        int j;
-         for(int j=i-1;j>=0;j--){
-            if(arr[j]>arr[i]){
-                cout<<arr[j]<<" ";
-                break;
-            }
-         }
-         if(j==-1){
-            cout<<"-1 ";
-         }
-    }
-}
-int main(){
-    vector<int>arr{15,10,18,12,4,6,2,8};
-    prevoiusGreater(arr);
-    return 0;
-}
+// #include<bits/stdc++.h>
+// using namespace std;
+// void prevoiusGreater(vector<int>&arr){
+//     int n=arr.size();
+//     for(int i=0;i<n;i++){
+//         int j;
+//          for(int j=i-1;j>=0;j--){
+//             if(arr[j]>arr[i]){
+//                 cout<<arr[j]<<" ";
+//                 break;
+//             }
+//          }
+//          if(j==-1){
+//             cout<<"-1 ";
+//          }
+//     }
+// }
+// int main(){
+//     vector<int>arr{15,10,18,12,4,6,2,8};
+//     prevoiusGreater(arr);
+//     return 0;
+// }
 
 
 // best solution
 
-#include<bits/stdc++.h>
+//Naive solution O(n^2)
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// vector<int>prevGreater(vector<int>&v){
+//     vector<int>res;
+//     int next=1;
+//     res.push_back(-1);
+//     while(next<v.size()){
+//         int prev=next-1;
+//         while(v[prev]<v[next]){
+//             prev--;
+//         }
+//         if(prev<0){
+//             res.push_back(-1);
+//         }else{
+//             res.push_back(v[prev]);
+//         }
+//         next++;
+//     }
+//     return res;
+// }
+// int main(){
+//     // vector<int>v{15,10,18,12,4,6,2,8};
+//     // vector<int>v{8,10,12};
+//     vector<int>v{12,10,8};
+//     vector<int>result=prevGreater(v);
+//     for(auto it:result){
+//         cout<<it<<" ";
+//     }
+//     return 0;
+// }
+
+//Efficient solution O(n)
+
+#include<iostream>
+#include<vector>
+#include<stack>
 using namespace std;
-void previousGreater(vector<int>&arr){
-    int n=arr.size();
+void PrevGreater(vector<int>&v){
     stack<int>s;
-    s.push(arr[0]);
-    for(int i=0;i<n;i++){
-        while(!s.empty() && arr[i]>=s.top()){
+    s.push(v[0]);
+    for(int i=0;i<v.size();i++){
+        while(!s.empty() && s.top()<=v[i]){
             s.pop();
         }
-        int res=(s.empty())?-1:s.top();
-        cout<<res<<" ";
-        s.push(arr[i]);
+        int x=s.empty()?-1:s.top();
+        cout<<x<<" ";
+        s.push(v[i]);
     }
 }
 int main(){
-    vector<int>arr{20,10,24,12,8,9};// -1,20,-1,24,12,12
-    previousGreater(arr);
+    vector<int>v{15,10,18,12,4,6,2,8};
+    PrevGreater(v);
     return 0;
 }
