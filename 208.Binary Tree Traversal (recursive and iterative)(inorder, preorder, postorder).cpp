9bc80bd62899bfd0a787ebf00b491bc7b49847b4
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-08-24 14:19:14 +0530

    208.Binary Tree Traversal (recursive and iterative)(inorder, preorder, postorder).cpp

--- 208.Binary Tree Traversal (recursive and iterative)(inorder, preorder, postorder).cpp
+++ 208.Binary Tree Traversal (recursive and iterative)(inorder, preorder, postorder).cpp
@@ -32,6 +32,7 @@ void inorder_iterative(node *root){
         curr=curr->right;
     }
 }
+// recursive preorder traversal
 void preorder(node *root){
     if(root!=NULL){
         cout<<root->key<<" ";
@@ -39,6 +40,40 @@ void preorder(node *root){
         preorder(root->right);
     }
 }
+//iterative preorder traversal in O(n) time and space 
+// void preorder_iterative(node *root){
+//     if(root==NULL)return;
+//     stack<node*>s;
+//     s.push(root);
+//     while(!s.empty()){
+//         node* curr=s.top();
+//         cout<<curr->key<<" ";
+//         s.pop();
+//         if(curr->right!=NULL)s.push(curr->right);
+//         if(curr->left!=NULL)s.push(curr->left);
+//     }
+// }
+//iterative preorder traversal in O(n) time and space O(h)
+void preorder_iterative(node *root){
+    if(root==NULL)return;
+    stack<node*>s;
+    s.push(root);
+    node* curr= root;
+     while(curr!=NUll||!s.empty()){
+        while (curr!=NULL){
+             cout<<curr->key<<" ";
+             if(curr->right!=NULL)s.push(curr->right);
+             curr=curr->left;
+        }
+        if(!s.empty()){
+            curr=s.top();
+            s.pop();
+        }
+        
+     }
+}
+
+
 void postorder(node *root){
     if(root!=NULL){
         postorder(root->left);
@@ -58,7 +93,8 @@ int main(){
     cout<<"Inorder Traversal: ";
     inorder(root);
     cout<<"\nPreoder Traversal: ";
-    preorder(root);
+    // preorder(root);
+    preorder_iterative(root);
     cout<<"\nPostoder Traversal: ";
     postorder(root);
     return 0;
