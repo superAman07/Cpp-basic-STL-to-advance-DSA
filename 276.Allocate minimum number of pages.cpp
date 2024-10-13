586f6b4190453ab75e82f0baa36455edb7587652
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-10-14 00:36:22 +0530

    Create 276.Allocate minimum number of pages.cpp

---
+++ 276.Allocate minimum number of pages.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int sum(int arr[],int b,int e){
+    int s=0;
+    for(int i=b;i<=e;i++){
+        s+=arr[i];
+    }
+    return s;
+}
+int minPages(int arr[],int n,int k){
+    int dp[k+1][n+1];
+    for(int i=1;i<=n;i++)dp[1][i]=sum(arr,0,i-1);
+    for(int i=1;i<=k;i++)dp[i][1]=arr[0];
+    for(int i=2;i<=k;i++){
+        for(int j=2;j<=n;j++){
+            int res = INT_MAX;
+            for(int p=1;p<j;p++){
+                res = min(res,max(dp[i-1][p],sum(arr,p,j-1)));
+            }
+            dp[i][j] = res;
+        }
+    }
+    return dp[k][n];
+}
+int main(){
+    int n=4;
+    int arr[n]{10,20,30,40};
+    int k=2;
+    cout<<minPages(arr,n,k);
+    return 0;
+}
