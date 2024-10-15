aa31b06f5d8bc087bee8bfb4de05454951975e65
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-10-15 22:34:02 +0530

    Update and rename 277.Trie Representation (insert , search).cpp to 277.Trie Representation (insert , search, delete).cpp

--- 277.Trie Representation (insert , search, delete).cpp
+++ 277.Trie Representation (insert , search, delete).cpp
@@ -33,6 +33,32 @@ void insert(string &key){
     }
     curr->isEnd=true;
 }
+// Delete from dictionary in O(n) time
+bool isEmpty(TrieNode *root){
+    for(int i=0;i<26;i++){
+        if(root->child[i]!=NULL)return false;
+    }
+    return  true;
+}
+TrieNode *delKey(TrieNode*root,string &key,int i){
+    if(root==NULL)return NULL;
+    if(i==key.length()){
+        root->isEnd=false;
+        if(isEmpty(root)==true){
+            delete root;
+            root==NULL;
+        }
+        return root;
+    }
+    int index = key[i]-'a';
+    root->child[index]=delKey(root->child[index],key,i);
+    if(isEmpty(root)&&root->isEnd==false){
+        delete root;
+        root==NULL;
+    }
+    return root;
+}
+
 int main(){
     string str="bad";
     insert(str);
